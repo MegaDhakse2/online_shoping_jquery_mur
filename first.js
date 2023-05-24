78ae5113c8594e3d3bec97b933082b4ef2feb693
MegaDhakse2 <meghanadhan@e2infosystems.com>
2023-05-24 20:17:31 +0530

    created form

--- first.js
+++ first.js
@@ -7,7 +7,33 @@ class Car {
 }
 
 
+var product_name;
+var product_price;
+var quantity;
+
 $(document).ready(function(){
-    $('a').attr("href","user_form.html")
-    .html(' <span class="trackName">Track Name</span>');
+    // $('a').attr("href","user_form.html")
+    // .html(' <span class="trackName">Track Name</span>');
+
+    $("#user_submission").click(function(){{
+         product_name = $("#product_name").val();
+         product_price = $("#product_price").val();
+         quantity = $("#quantity").val();
+         console.log(product_name + "\n" + product_price +  "\n" + quantity);
+         window.location.replace("first.html");
+         $(".main_div").text("klfjkdlj");
+    }});
+
+
 });
+
+
+function method(){
+    $(".main_div").text("klfjkdlj");
+}
+// $(document).ready(function(){
+//     $("#user_submission").click(function(){{
+//         let quantity = $("#quantity").val();
+//        alert(quantity);
+//     }});
+// });
